46b8ae5650f1e3c7fc223816bfe2f2aebeb059f8
Allison Tao <allisontao04@gmail.com>
2024-07-22 18:10:35 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function updateClock() {
+
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const min = now.getMinutes().toString().padStart(2, 0);
+    const sec = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${min}:${sec}`;
+    document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000); // call fn after every 1000ms
